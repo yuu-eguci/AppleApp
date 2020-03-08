2a70109ccdfa4eca4de3c3d93b09d33b05b0fe43
yuu-eguci <yuu.eguci@gmail.com>
2020-03-08 20:23:03 +0900

    Create ASP.NET MVC project

---
+++ AppleApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AppleApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AppleApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppleApp.MvcApplication" Language="C#" %>
